b7fff4820fcd952d6616f6a32d3c2da0cc6d3e29
jin <59611464+YJYeun@users.noreply.github.com>
2020-03-18 00:11:24 +0900

    Update Main.java

--- Tutorial1/src/Main.java
+++ Tutorial1/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in); // System.in 콘솔창에서 입력하는 어떠한 데이터같은것을 의미함
+		Scanner sc = new Scanner(System.in); 
 		System.out.println("정수를 입력하세요 : ");
 		int i = sc.nextInt();
 		System.out.println("입력된 정수"+ i + "입니다");
